abab4e7cf0af0526dcee69f0559a7ed0cdebd37f
doktorjohan <johan.hollak@ut.ee>
2025-01-31 15:19:55 +0200

    moving to vercel

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,30 +11,18 @@
 // Composables
 import { createRouter } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
-import {createWebHashHistory} from "vue-router";
+import {createWebHistory} from "vue-router";
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+  history: createWebHistory(),
+  routes
 })
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
-    } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
-    }
-  } else {
-    console.error(err)
+  if (err.message.includes('Failed to fetch dynamically imported module') || err.message.includes("Importing a module script failed")) {
+    window.location = to.fullPath
   }
-})
-
-router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
 })
 
 export default router
--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -11,7 +11,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/male/",
   plugins: [
     VueRouter(),
     Vue({
