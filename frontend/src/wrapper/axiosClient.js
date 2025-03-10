3235b202028ef4ca1d38e228e5a446dd3e787499
johan.hollak <johanhollak1@gmail.com>
2025-03-10 11:25:38 +0200

    work in cloud for now

--- frontend/src/wrapper/axiosClient.js
+++ frontend/src/wrapper/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://male-be.onrender.com/api',
   timeout: 30000,
   headers: {'Application-Type': 'application/json'}
 });
